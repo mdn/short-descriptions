a20bae93e0e9e40a584260195fd25df49a59ec4d
Daniel D. Beck <daniel@ddbeck.com>
2018-11-26 12:01:21 +0000

    Fix autofixable linter errors

--- test/lint-short-descriptions.js
+++ test/lint-short-descriptions.js
@@ -17,10 +17,10 @@ const properties = require('mdn-data').css.properties;
 const lengthLimit = 180;
 const firstSentenceLengthLimit = 150;
 const allowed = {
-  'A': ['href'],
-  'CODE': [],
-  'EM': [],
-  'STRONG': [],
+  A: ['href'],
+  CODE: [],
+  EM: [],
+  STRONG: [],
 };
 
 const cli = async () => {
@@ -38,23 +38,21 @@ const main = async (args) => {
         prop = 'standard input';
         url = 'no URL';
         summary = await readDataFromStdin();
-      }
-      else {
+      } else {
         url = nameToURL(prop);
         summary = await readDataFromURL(url);
       }
 
       checkSummary(summary, prop, url);
     }
-  }
-  catch (e) {
+  } catch (e) {
     console.trace(e);
   }
 };
 
 const test = () => {
-  const exampleOK = `The <strong><code>color</code></strong> CSS property sets the foreground <a href="https://developer.mozilla.org/docs/Web/CSS/color_value">color value</a> of an element's text and <a href="https://developer.mozilla.org/docs/Web/CSS/text-decoration">text decorations</a>. It also sets the <a href="https://developer.mozilla.org/docs/Web/CSS/currentcolor"><code>currentcolor</code></a> value, an indirect value on <em>other</em> properties.`;
-  const exampleNotOK = `The <strong><code>color</code></strong> CSS property sets the foreground <a href="/en-US/docs/Web/CSS/color_value">color value</a> of an element's text content and <a href="/en-US/docs/Web/CSS/text-decoration" name='notallowed'>text decorations</a> and also this sentence is much too long to be the first sentence of a short description. And the whole thing should be less than 180 characters. <span>And this enclosing span tag is not allowed</span>. And neither is hidden br tag <br/>.`;
+  const exampleOK = 'The <strong><code>color</code></strong> CSS property sets the foreground <a href="https://developer.mozilla.org/docs/Web/CSS/color_value">color value</a> of an element\'s text and <a href="https://developer.mozilla.org/docs/Web/CSS/text-decoration">text decorations</a>. It also sets the <a href="https://developer.mozilla.org/docs/Web/CSS/currentcolor"><code>currentcolor</code></a> value, an indirect value on <em>other</em> properties.';
+  const exampleNotOK = 'The <strong><code>color</code></strong> CSS property sets the foreground <a href="/en-US/docs/Web/CSS/color_value">color value</a> of an element\'s text content and <a href="/en-US/docs/Web/CSS/text-decoration" name=\'notallowed\'>text decorations</a> and also this sentence is much too long to be the first sentence of a short description. And the whole thing should be less than 180 characters. <span>And this enclosing span tag is not allowed</span>. And neither is hidden br tag <br/>.';
 
   console.log('-- Testing OK text --');
   checkSummary(exampleOK, 'color', 'https://developer.mozilla.example/thisIsNotARealURL');
@@ -68,7 +66,7 @@ const checkSummary = (summaryData, propertyName, url) => {
     checkLength,
     checkFirstSentenceLength,
     checkTags,
-    checkAttrs
+    checkAttrs,
   ];
 
   let ok = true;
@@ -78,7 +76,7 @@ const checkSummary = (summaryData, propertyName, url) => {
   const summaryText = summaryDom.window.document.querySelector('body').textContent;
 
   for (const check of checks) {
-    const {status, errors} = check(propertyName, summaryText, summaryDom);
+    const { status, errors } = check(propertyName, summaryText, summaryDom);
     if (!status) {
       ok = false;
       messages.push(...errors);
@@ -87,10 +85,9 @@ const checkSummary = (summaryData, propertyName, url) => {
 
   if (ok) {
     console.log(`✅ \x1b[1m${propertyName}\x1b[0m (${url}) is OK`);
-  }
-  else {
+  } else {
     console.error(`❌ \x1b[1m${propertyName}\x1b[0m (${url}) has problems`);
-    messages.forEach((value) => console.log(value));
+    messages.forEach(value => console.log(value));
   }
 };
 
@@ -110,9 +107,9 @@ const nameToURL = (property) => {
   return `${properties[property].mdn_url}?raw&summary&${cacheBuster}`;
 };
 
-const readDataFromStdin = async () => {
+const readDataFromStdin = async () => 
   // read text from standard input (returns a Promise)
-  return new Promise((resolve, reject) => {
+   new Promise((resolve, reject) => {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
@@ -122,23 +119,23 @@ const readDataFromStdin = async () => {
 
     rl.on('line', (line) => lines.push(line));
     rl.on('close', () => resolve(lines));
-  });
-};
+  })
+;
 
-const readDataFromURL = async (url) => {
+const readDataFromURL = async (url) => 
   // fetch URL (returns a Promise)
-  return new Promise((resolve, reject) => {
+   new Promise((resolve, reject) => {
     request.get(url, (error, response, body) => {
       error ? reject(error) : {};
       resolve(body);
     });
-  });
-};
+  })
+;
 
 const checkLength = (propertyName, summaryText, summaryDom) => {
   if (isLengthOK(summaryText)) {
     return {status: true};
-  } else {
+  } 
     return {
       status: false,
       errors: [
@@ -146,14 +143,14 @@ const checkLength = (propertyName, summaryText, summaryDom) => {
         `       > ${summaryText.slice(0,180)}\x1b[41m${summaryText.slice(180)}\x1b[0m`
       ]
     };
-  }
+  
 };
 
 const checkFirstSentenceLength = (propertyName, summaryText, summaryDom) => {
   const sentence = firstSentence(summaryText);
   if (isFirstSentenceLengthOK(summaryText)) {
     return {status: true};
-  } else {
+  } 
     return {
       status: false,
       errors: [
@@ -161,48 +158,48 @@ const checkFirstSentenceLength = (propertyName, summaryText, summaryDom) => {
         `       > ${sentence.slice(0, firstSentenceLengthLimit)}\x1b[41m${sentence.slice(firstSentenceLengthLimit)}\x1b[0m`
       ]
     };
-  }
+  
 };
 
 const checkTags = (propertyName, summaryText, summaryDom) => {
   const tagSet = getTagSet(summaryDom);
   if (areTagsOK(tagSet)) {
     return {status: true};
-  } else {
+  } 
     return {
       status: false,
       errors: [
         `    ❌ ${propertyName} summary contains forbidden tags: ${forbiddenTags(tagSet).join(', ')}\x1b[0m`
       ]
     };
-  }
+  
 };
 
 const checkAttrs = (propertyName, summaryText, summaryDom) => {
   if (areAttrsOK(summaryDom)) {
     return {status: true};
-  } else {
+  } 
     return {
       status: false,
       errors: [
         `    ❌ ${propertyName} summary contains forbidden attributes: ${forbiddenAttrs(summaryDom).join(', ')}\x1b[0m`
       ]
     };
-  }
+  
 };
 
-const isLengthOK = (text) => lengthLimit >= text.length;
+const isLengthOK = text => lengthLimit >= text.length;
 
-const isFirstSentenceLengthOK = (text) => firstSentenceLengthLimit >= firstSentence(text).length;
+const isFirstSentenceLengthOK = text => firstSentenceLengthLimit >= firstSentence(text).length;
 
-const firstSentence = (text) => {
+const firstSentence = (text) => 
   // a very simplistic attempt to match the first sentence of the summary
-  return text.replace(/\.(?!\d)/g, '.\x1f').split('\x1f')[0];
-};
+   text.replace(/\.(?!\d)/g, '.\x1f').split('\x1f')[0]
+;
 
-const areTagsOK = (tagSet) => forbiddenTags(tagSet).length === 0;
+const areTagsOK = tagSet => forbiddenTags(tagSet).length === 0;
 
-const forbiddenTags = (tagSet) => Array.from(tagSet).filter(value => !Object.keys(allowed).includes(value));
+const forbiddenTags = tagSet => Array.from(tagSet).filter(value => !Object.keys(allowed).includes(value));
 
 const getTagSet = (dom) => {
   const tagSet = new Set();
@@ -212,10 +209,10 @@ const getTagSet = (dom) => {
   return tagSet;
 };
 
-const areAttrsOK = (dom) => 0 === forbiddenAttrs(dom).length;
+const areAttrsOK = dom => forbiddenAttrs(dom).length === 0;
 
 const forbiddenAttrs = (dom) => {
-  let badAttrs = [];
+  const badAttrs = [];
 
   for (const elem of dom.window.document.querySelectorAll('BODY *')) {
     const allowedAttrs = allowed[elem.tagName];
